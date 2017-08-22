7bd72257cf9ca78cf5d7c311121f6fd412192e71
Dustin21 <dustin.bwj@gmail.com>
2017-08-22 13:36:20 -0700

    first dtw commit

---
+++ dtw.cpp
@@ -0,0 +1,9 @@
+//
+//  dtw.cpp
+//  DynamicTimeWarp
+//
+//  Created by Dustin on 2017-08-22.
+//  Copyright © 2017 Dustin Johnson. All rights reserved.
+//
+
+#include "dtw.hpp"
---
+++ dtw.hpp
@@ -0,0 +1,14 @@
+//
+//  dtw.hpp
+//  DynamicTimeWarp
+//
+//  Created by Dustin on 2017-08-22.
+//  Copyright © 2017 Dustin Johnson. All rights reserved.
+//
+
+#ifndef dtw_hpp
+#define dtw_hpp
+
+#include <stdio.h>
+
+#endif /* dtw_hpp */
